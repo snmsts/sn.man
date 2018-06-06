29f85a71d3e64b85430dbe39db7b4eda12765d7a
SANO Masatoshi <snmsts@gmail.com>
2018-06-06 12:57:01 +0900

    first commit

---
+++ main.lisp
@@ -0,0 +1,22 @@
+(uiop/package:define-package :sn.man/main (:nicknames :sn.man) (:use :cl)
+                             (:shadow) (:export :man) (:intern))
+(in-package :sn.man/main)
+;;don't edit above
+
+(defun which (command)
+  ;;tbd test on windows.
+  (string-right-trim
+   (format nil "~%")
+   (with-output-to-string (o)
+     (uiop:run-program (format nil (or #+windows "cmd /c where ~A"
+                                       "command -v ~A")
+                               command) :ignore-error-status t :output o))))
+
+(defparameter *which-man* nil)
+
+(defun man (entry &key output-format)
+  (declare (ignore output-format))
+  (when (or *which-man*
+            (setf *which-man* (which "man")))
+    (with-output-to-string (o)
+      (uiop:run-program (format nil "man ~S" entry) :output o))))
---
+++ sn.man.asd
@@ -0,0 +1,4 @@
+;;don't edit
+(DEFSYSTEM "sn.man" :CLASS :PACKAGE-INFERRED-SYSTEM :COMPONENTS
+ ((:FILE "main")) :LICENSE "mit" :AUTHOR "SANO Masatoshi" :MAILTO
+ "snmsts@gmail.com")
